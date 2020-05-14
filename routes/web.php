789e0704495275ab189e424082771019dfdbafa1
Ambar Quintana <ambarsquintana@gmail.com>
2020-05-14 19:43:18 -0400

    add routes

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('notas', function () {
     return 'Aquí estará el listado para crear notas';
 });
 
 Route::get('notas/crear', function () {
     return 'Aquí estará el formulario para crear notas';
+});
+
+Route::get('notas/{id}', function ($id) {
+   return "Aquí estará el detalle de la nota ".$id;
+});
+
+Route::get('notas/{id}/editar', function ($id) {
+   return "Aquí estará el formulario para editar la nota ".$id;
 });
