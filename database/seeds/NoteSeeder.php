0fc96e0fc932f7441a706f4b1e8bf1b0b5c3bb7f
Ambar Quintana <ambarsquintana@gmail.com>
2020-05-17 00:39:22 -0400

    add note seeder

---
+++ database/seeds/NoteSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class NoteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('notes')->insert([
+            'title' => '¿Para qué sirve Composer?',
+            'content' => 'Con Composer podemos instalar y actualizar frameworks como Laravel o Symfony, así como componentes para generar PDF, procesar pagos con tarjetas, manipular imágenes y mucho más.',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('notes')->insert([
+            'title' => 'Instalación de Laravel',
+            'content' => File::get(__DIR__.'/notes/installation.html'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('notes')->insert([
+            'title' => 'Rutas y JSON',
+            'content' => File::get(__DIR__.'/notes/routes.html'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('notes')->insert([
+            'title' => 'Front Controller',
+            'content' => 'Front Controller es un patrón de arquitectura donde un controlador maneja todas las solicitudes o peticiones a un sitio web.',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('notes')->insert([
+            'title' => 'Cambia el formato de parámetros dinámicos',
+            'content' => File::get(__DIR__.'/notes/parameters.html'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
