0d89a0a8c001abee5d579caa9f135c76ad81d1f3
Ambar Quintana <ambarsquintana@gmail.com>
2020-05-18 21:57:00 -0400

    update edit view

--- resources/views/edit-note.blade.php
+++ resources/views/edit-note.blade.php
@@ -9,14 +9,34 @@
                 <div class="card-body">
                     <h1>Editar nota</h1>
 
-                    <form action="">
+                    @if ($errors->any())
+                        <div class="errors">
+                            <p><strong>Por favor corrige los siguientes errores:</strong></p>
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <form method="POST" action="{{ url("notas/{$note->id}") }}">
+                        @csrf
+                        @method('PUT')
+
                         <label for="title" class="field-label">Título: </label>
-                    <input type="text" name="title" id="title" class="field-input" value="{{ $note->title }}">
+                        <input type="text" name="title" id="title" value="{{ old('title', $note->title) }}" class="field-input @error('title') field-error @enderror ">
+                        @error('title')
+                            <p class="error-message">{{ $message }}</p>
+                        @enderror
 
                         <label for="content" class="field-label">Contenido:</label>
-                        <textarea name="content" id="content" rows="10" class="field-textarea">{!! $note->content !!}</textarea>
+                        <textarea name="content" id="content" rows="10" class="field-textarea @error('content') field-error @enderror">{{ old('content', $note->content) }}</textarea>
+                        @error('content')
+                            <p class="error-message">{{ $message }}</p>
+                        @enderror
 
-                        <button type="submit" class="btn btn-primary">Editar nota</button>
+                        <button type="submit" class="btn btn-primary">Actualizar nota</button>
                     </form>
                 </div>
             </div>
